816282a59dadd6496a3763d66d47e867e115a5bd
harshparticle <152156072+harshparticle@users.noreply.github.com>
2025-05-05 23:38:02 -0700

    Add files via upload

--- huang.py
+++ huang.py
@@ -1,6 +1,19 @@
+##This code has been written to compute Huang-Rhys factor from output file of ezFCF. It calculates Huang rhys factor as 0.5*omega_i*(dq_i)^2
+##The code can be run as python3 huang.py/<code_name.py> <path_file> 
+##Huang-Rhys Factor are essential to understand franck condon active modes in the molecule.
+
 #!/usr/bin/env python3
 import sys
 
+# constants
+hbar = 1.0545718e-34          # J·s
+c    = 2.99792458e10          # cm/s
+amu  = 1.660539e-27  
+amu_to_kg = 1.66053906660e-27       # kg per amu
+me_kg     = 9.1093837015e-31        # kg per electron mass
+angstrom_to_bohr = 1.0/0.529177210903e-10  # Å → a₀ (in m/m)
+
+#Parsing code to extract data after the keyword "Begining the Parallel mode computations" from ezFCF output
 def parse_after_keyword(file_path, keyword):
     parsed_data = []
     with open(file_path, 'r', errors='ignore') as f:
@@ -38,12 +51,12 @@ if __name__ == "__main__":
     keyword = "Beginning the parallel mode approximation computations."
     data = parse_after_keyword(path, keyword)
 
-    # report and preview
+    # Parsing the whole table from ezFCF output, length of data you want to parse can be adjusted according to problem
     print(f"Parsed {len(data)} rows after the keyword.\n")
     for row in data[7:55]:
         print(row)
 
-    # example NumPy conversion:
+    # Converting Lists into array.
     try:
         import numpy as np
         arr = np.array(data[7:55], dtype=float)
@@ -51,26 +64,19 @@ if __name__ == "__main__":
     except Exception as e:
         print("\nNumPy conversion failed:", e)
 
+    #Extracting displacements and frequency for Huang-Rhys factor Calculation.
     dq=arr[:,1]
     freq=arr[:,3]
-    
-    # constants
-    hbar = 1.0545718e-34          # J·s
-    c    = 2.99792458e10          # cm/s
-    amu  = 1.660539e-27  
-    amu_to_kg = 1.66053906660e-27       # kg per amu
-    me_kg     = 9.1093837015e-31        # kg per electron mass
-    angstrom_to_bohr = 1.0/0.529177210903e-10  # Å → a₀ (in m/m)
 
-    # compute the conversion factor
-    factor = np.sqrt(amu_to_kg/me_kg) * angstrom_to_bohr         # kg
-
+    #Converting displacement from Angstorm*sqrt(amu) to m*sqrt(kg) and atomic units.
     dq_SI   = dq * 1e-10* np.sqrt(amu)
-    omega = 2*np.pi * c * freq
-
     dq_au=dq * (1.889716) * (np.sqrt(1822.89))
+    
+    #Converting frequency from cm^-1 to rad/s and atomic units.
+    omega = 2*np.pi * c * freq
     omega_au=freq * (1/8065.579) * (1/27.211386245988)
 
+    #Computing Huang-Rhys Factor
     S = omega * dq_SI**2 / (2 * hbar) 
     S_au=0.5 * omega_au * dq_au**2
 
